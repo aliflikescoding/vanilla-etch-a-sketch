e3edb6abdba911403e57d96c83faf1167fc8bfdc
Alif Wibowo <alif.likes.coding@gmail.com>
2022-10-05 11:33:29 +0700

    Add erase function to etch a sketch

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
+
+const gridHeight = 20;
+const gridWidth = 40;
+
 /* Generate Grids */
 const gridArea = document.querySelector('.grid-area');
-for (let i = 1; i <= 40; i++) {
-    const gridRow = document.createElement('div');
-    for (let j = 1; j <= 20; j++) {
+for (let i = 1; i <= gridWidth; i++) {
+    const gridCol = document.createElement('div');
+    gridCol.classList.add('gridCol');
+    for (let j = 1; j <= gridHeight; j++) {
         const grid = document.createElement('div');
-        gridRow.appendChild(grid);
+        gridCol.appendChild(grid);
         grid.classList.add('grid');
     }
-    gridArea.appendChild(gridRow);
+    gridArea.appendChild(gridCol);
 }
+
+/* erase */
+const eraseButton = document.querySelector('#circle2');
+eraseButton.addEventListener('click', () => {
+    if (eraseButton.classList.contains('erase') == false) {
+        eraseButton.classList.add('erase');
+    }
+    else {
+        eraseButton.classList.remove('erase');
+    }
+});
+
+/* change fill if hovered */
+const grids = document.querySelectorAll('.grid');
+grids.forEach(grid => {
+    grid.addEventListener('mouseover', () => {
+        if (eraseButton.classList.contains('erase')) {
+            grid.classList.remove('grid-fill')
+        }
+        else {
+            grid.classList.add('grid-fill');
+        }
+    });
+});
 
